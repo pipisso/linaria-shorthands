7fc38194509de4339ad49e7c68363c76f4690592
Levin Uncu <levin@uncu.de>
2023-08-19 13:37:48 +0200

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+};
+
+module.exports = config
---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'avoid',
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: false,
+};
+
+module.exports = config;
