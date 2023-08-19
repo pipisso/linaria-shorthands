5a56af5d390a2ce70aa0f9754cb91e9c6c1907e7
Levin Uncu <levin@uncu.de>
2023-08-19 20:33:24 +0200

    bump version to v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,13 @@ All notable changes to the library will be documented in this file.
 
 ## vX.X.X (Month DD, YYYY)
 
+## v0.2.0 (August 19, 2023)
+
 - Added `margin` and `padding` shorthands.
 - Added `borderWidth` shorthand.
 - Added `borderStyle` shorthand.
 - Added `borderColor` shorthand.
-- Added `borderTop`, `borderBottom`, `borderRight` and `borderLeft` shorthand.
+- Added `borderTop`, `borderBottom`, `borderRight` and `borderLeft` shorthands.
 - Added `border` shorthand.
 
 ## v0.1.0 (August 19, 2023)
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import {margin} from './margin/margin';
 import {padding} from './padding/padding';
+import {border} from './border/border';
+import {borderTop} from './border/borderTop';
+import {borderBottom} from './border/borderBottom';
+import {borderLeft} from './border/borderLeft';
+import {borderRight} from './border/borderRight';
+import {borderColor} from './border/borderColor';
+import {borderStyle} from './border/borderStyle';
+import {borderWidth} from './border/borderWidth';
 
-export {margin, padding};
+export {
+  margin,
+  padding,
+  border,
+  borderBottom,
+  borderColor,
+  borderLeft,
+  borderRight,
+  borderStyle,
+  borderTop,
+  borderWidth,
+};
